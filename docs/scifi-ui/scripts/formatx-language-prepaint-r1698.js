3075b674b422528a8cc177f041bde2f27c82dfe3
Games Tech <hutoczky@users.noreply.github.com>
2026-09-23 23:33:59 +0200

    fix(web): set query language before first paint r1698

---
+++ docs/scifi-ui/scripts/formatx-language-prepaint-r1698.js
@@ -0,0 +1,1 @@
+(()=>{'use strict';const q=new URLSearchParams(location.search).get('lang');if(q==='hu'||q==='en'){document.documentElement.lang=q;document.documentElement.dataset.fxLanguagePrepaintR1698=q;}})();
